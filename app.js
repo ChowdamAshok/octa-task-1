188c99faee9fdfa448c7db9e345c379028c82676
ChowdamAshok <161301092+ChowdamAshok@users.noreply.github.com>
2024-04-04 11:24:30 +0530

    app.js

--- app.js
+++ app.js
@@ -1,21 +1,10 @@
 gsap.from('.logo',{opacity: 0, duration: 1, delay:2, y:10})
 gsap.from('.navbar a',{opacity: 0, duration: 1, delay:2.1, y:30, stagger:0.2})
+gsap.from('.social-icons img',{opacity: 0, duration: 1, delay:2.2, y:30, stagger:0.3})
 gsap.from('.left',{opacity: 0, duration: 1, delay:2, y:20, stagger:0.2})
 gsap.from('.right',{opacity: 0, duration: 1, delay:2, y:10})
-gsap.from('.left-2',{opacity: 0, duration: 1, delay:2, y:10})
-gsap.from('.right-2',{opacity: 0, duration: 1, delay:1.5, y:30, stagger:0.3})
-gsap.from('.cards', {
-    opacity: 0,
-    duration: 1,
-    delay: 2,
-    stagger: 1, 
-    onComplete: function() {
-      gsap.to('.cards', {
-        x: '0%', // Reset cards' position after animation completes
-        duration: 2,
-        delay: 1
-      });
-    }
-  });
+gsap.from('.about-img',{opacity: 0, duration: 1, delay:2, y:10})
+gsap.from('.about-text',{opacity: 0, duration: 1, delay:1.5, y:30, stagger:0.3})
+gsap.from('.cards',{opacity: 0, duration: 1, delay:2.2, y:30, stagger:0.3})
   
 
