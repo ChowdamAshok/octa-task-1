c3c663b18c4d2ca14b517d629d06e02b505e3807
ChowdamAshok <161301092+ChowdamAshok@users.noreply.github.com>
2024-04-06 15:48:15 +0530

    app.js

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 gsap.from('.logo',{opacity: 0, duration: 1, delay:2, y:10})
 gsap.from('.navbar a',{opacity: 0, duration: 1, delay:2.1, y:30, stagger:0.2})
-gsap.from('.social-icons img',{opacity: 0, duration: 1, delay:2.2, y:30, stagger:0.3})
 gsap.from('.left',{opacity: 0, duration: 1, delay:2, y:20, stagger:0.2})
 gsap.from('.right',{opacity: 0, duration: 1, delay:2, y:10})
 gsap.from('.about-img',{opacity: 0, duration: 1, delay:2, y:10})
 gsap.from('.about-text',{opacity: 0, duration: 1, delay:1.5, y:30, stagger:0.3})
-gsap.from('.cards',{opacity: 0, duration: 1, delay:2.2, y:30, stagger:0.3})
+
   
 
